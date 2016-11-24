e025ae27c9650e358f8539b135660f302c4bb51e
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2016-11-24 20:56:34 +0100

    Move TODOs from code to issues

--- examples/space-game-example.js
+++ examples/space-game-example.js
@@ -6,12 +6,10 @@ function handleControlClicked(control) {
   console.log('control:', control);
 }
 
-// TODO: replace unreadable task setting by parseWorldDescription(`b |bA|....`)
 const gameState= {
   fields: [[["b", []], ["b", ["A"]], ["b", ["M"]], ["b", ["A"]], ["b", []]], [["k", []], ["k", ["A"]], ["k", []], ["k", ["A"]], ["k", []]], [["k", []], ["k", ["A"]], ["k", ["M"]], ["k", ["A"]], ["k", []]], [["k", []], ["k", ["A"]], ["k", []], ["k", ["A"]], ["k", []]], [["k", []], ["k", ["A"]], ["k", ["M"]], ["k", ["A"]], ["k", []]], [["k", []], ["k", ["A"]], ["k", []], ["k", ["A"]], ["k", []]], [["k", []], ["k", ["A"]], ["k", ["M"]], ["k", ["A"]], ["k", []]], [["k", []], ["k", ["A"]], ["k", []], ["k", ["A"]], ["k", []]], [["k", []], ["k", ["A"]], ["k", ["S"]], ["k", ["A"]], ["k", []]]],
   stage: 'initial'
 }
-// TODO: default values, e.g. command controls to false
 const component = (
   <SpaceGame
     gameState={gameState}
