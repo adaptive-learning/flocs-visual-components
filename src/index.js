d56190d48c8ecfe5ba8df939ff3e5a4371ba1986
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2016-12-01 14:06:51 +0100

    Export also containers and dedicated reducer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import SpaceGame  from './components/SpaceGame';
 import CodeEditor from './components/CodeEditor';
+import SpaceGameContainer from './containers/SpaceGameContainer';
+import CodeEditorContainer from './containers/CodeEditorContainer';
+import { flocsComponentsReducer } from './reducers';
 
 export {
+  // components
   SpaceGame,
   CodeEditor,
+
+  // containers
+  SpaceGameContainer,
+  CodeEditorContainer,
+
+  // reducer
+  flocsComponentsReducer
 };
