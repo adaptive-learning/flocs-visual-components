4620c4540d56b71e43e95b7bb800a63bbccf79b5
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2017-03-01 09:08:18 +0100

    Add example of space game container usage

--- examples/index.js
+++ examples/index.js
@@ -2,5 +2,6 @@ import './space-game-example';
 import './code-editor-example';
 import './blockly-editor-example';
 import './tasks-table-example';
+import './space-game-container-example';
 import './task-environment-example';
 import './task-editor-example';
