c62e80a1fd5ad65ae2cbe37a1d9450a96c3a9306
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2017-03-28 21:10:07 +0200

    Add student reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 import reduceTaskEnvironments from './taskEnvironments';
 import reduceTaskEditor from './taskEditor';
 import { reduceInstructions, reduceInstructionLayer } from './instructions';
+import { reduceStudent } from './student';
 
 export const flocsComponentsReducer = combineReducers({
+  student: reduceStudent,
   taskEnvironments: reduceTaskEnvironments,
   taskEditor: reduceTaskEditor,
   instructions: reduceInstructions,
